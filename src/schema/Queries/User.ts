6589e17b4a7ac0ed2889b261c35915252caca9ed
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-01-13 16:43:10 +0200

    getting one user  by id

--- src/schema/Queries/User.ts
+++ src/schema/Queries/User.ts
@@ -1,12 +1,19 @@
-import { GraphQLList, GraphQLString } from "graphql";
+import { GraphQLID, GraphQLList, GraphQLString } from "graphql";
 import { Users } from "../../Entities/Users";
 import { UserType } from "../typeDef/User";
 export const GET_ALL_USERS = {
   type: new GraphQLList(UserType),
   async resolve() {
-    return  await Users.find();
- 
+    return await Users.find();
+  },
+};
 
-    
+export const GET_USER = {
+  type: UserType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(_: any, args: any) {
+    return await Users.findOne(args.id);
   },
 };
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,7 @@
 import { GraphQLSchema, GraphQLObjectType } from "graphql";
 import { CREATE_USER } from "./Mutations/User";
 import { GREETING } from "./Queries/Greeting";
-import { GET_ALL_USERS } from "./Queries/User";
+import { GET_ALL_USERS, GET_USER } from "./Queries/User";
 
 //*Consulta Principal
 
@@ -11,6 +11,7 @@ const RootQuery = new GraphQLObjectType({
     //* Que es lo que tiene que devolver
     greeting: GREETING,
     getAllUsers: GET_ALL_USERS,
+    getUser: GET_USER,
   },
 });
 
